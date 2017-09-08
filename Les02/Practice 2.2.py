8a6c3ba4cb16d5de82ae792e7d009344063f9c57
johnvanmeerten <john.vanmeerten@student.hu.nl>
2017-09-08 15:58:33 +0200

    update

--- Les02/Practice 2.2.py
+++ Les02/Practice 2.2.py
@@ -6,3 +6,10 @@ gemiddelde = (cijferCSN+cijferICOR+cijferPROG)/3
 
 print(gemiddelde)
 
+totaal = (cijferCSN+cijferICOR+cijferPROG)*30
+
+print(totaal)
+
+overzicht = ('Mijn cijfer (gemiddeld een ' + str(gemiddelde)  + ') leveren een beloning van €' + str(totaal) + ' op!')
+
+print(overzicht)
