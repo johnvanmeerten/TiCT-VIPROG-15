4b233fd7b249d0e047c2e9540f830511d021ca76
johnvanmeerten <john.vanmeerten@student.hu.nl>
2017-09-08 13:31:35 +0200

    12

---
+++ Les02/Perkovic 2.31.py
@@ -0,0 +1,6 @@
+Extend plaats een 2e lijst eraan vast
+
+copy maakt een kopie van de benoemde lijst
+
+clear maakt de lijst leeg
+
---
+++ Les02/Perkovic 3.17.py
@@ -0,0 +1,14 @@
+a = eval('2*3+1')
+b = eval('hello')
+c = eval("'hello' + ' ' + 'world!'")
+d = eval("'ASCII'.count('I')")
+e = eval('x = 5')
+
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+
+b heeft geen waarden
+e variabel kan je geen variabel van maken
