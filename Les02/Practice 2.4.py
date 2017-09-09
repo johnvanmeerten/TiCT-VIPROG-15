3207bb0e63218b0ba372a18e55318aa7cf81aa66
johnvanmeerten <john.vanmeerten@student.hu.nl>
2017-09-09 12:23:47 +0200

    122

---
+++ Les02/Practice 2.4.py
@@ -0,0 +1,6 @@
+uurloon = eval(input('Wat verdien je per uur: '))
+aantal = eval(input('Hoeveel uur heb je gewerkt: '))
+
+totaal = uurloon*aantal
+
+print (str(aantal) + ' uur werken levert ' + str(totaal) + ' Euro op')
