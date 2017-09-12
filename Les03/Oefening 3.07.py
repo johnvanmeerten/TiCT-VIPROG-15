abceb6bb8295e40611da87c42c8648afaf5a2e69
johnvanmeerten <john.vanmeerten@student.hu.nl>
2017-09-12 11:52:34 +0200

    122

---
+++ Les03/Oefening 3.07.py
@@ -0,0 +1,18 @@
+for i in range(11):
+    print(i)
+
+for i in range(1, 10):
+    print(i)
+
+for i in range(0, 9, 2):
+    print(i)
+
+for i in range(1, 10, 2):
+    print(i)
+
+for i in range(20, 61, 10):
+    print(i)
+
+    # start eerst tenzij 0 dan niet niet opgeven en stap grote 1
+    # 2 is eind getal +1
+    # 3e is de stap grote opgeven tenzij stap grote 1 is
