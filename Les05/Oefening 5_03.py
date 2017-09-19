6fe2204de4c68b986a979e06100ac45d1653173a
johnvanmeerten <john.vanmeerten@student.hu.nl>
2017-09-19 10:45:18 +0200

    122

--- Les05/Oefening 5_03.py
+++ Les05/Oefening 5_03.py
@@ -2,3 +2,4 @@ naam = 'Mark Rutte'
 plaats = 'Den Haag'
 
 print('Mijn naam is ' + naam + ' en ik woon in '+ plaats)
+print('Mijn naam is {} en ik woon in {} ' .format(naam, plaats))
---
+++ Les05/Oefening 5_04.py
@@ -0,0 +1,15 @@
+leeftijd1 = 17
+leeftijd2 = 18
+
+som = leeftijd2 + leeftijd2
+
+print('De som van de leeftijden is {}' .format(som))
+
+weekdag = 'dinsdag'
+dag = '25'
+maand = 'maart'
+uur = 14
+minuten = 15
+
+print('{} {} {}'.format(weekdag, dag, maand))
+print('{} {} {} om {}.{} uur'.format(weekdag, dag, maand, uur, minuten))
