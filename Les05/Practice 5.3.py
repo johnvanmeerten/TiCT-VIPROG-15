e61179468bcb9fd2c2e05574e8cf76bf1d50004a
johnvanmeerten <john.vanmeerten@student.hu.nl>
2017-09-19 16:22:32 +0200

    122

---
+++ Les05/Practice 5.3.py
@@ -0,0 +1,13 @@
+infile = open('kaartnummers.txt', 'r')
+inhoud = infile.readlines()
+infile.close()
+
+hoogste = 0
+
+for regel in inhoud:
+     kaartinfo = regel.split(',')
+     if kaartinfo[0].strip() > hoogste:
+         hoogste = kaartinfo[0].strip()
+     print(hoogste)
+
+
