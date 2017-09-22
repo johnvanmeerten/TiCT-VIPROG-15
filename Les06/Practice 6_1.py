fb87a08eb758d027a11d26ecff4636a9422c7d4f
johnvanmeerten <john.vanmeerten@student.hu.nl>
2017-09-22 12:48:13 +0200

    cvbcvb

--- Les06/Practice 6_1.py
+++ Les06/Practice 6_1.py
@@ -1,2 +1,13 @@
-def seizoen ():
-    if getal >
+def seizoen (getal):
+    if getal >= 3 and getal <= 5:
+        return ('lente')
+    elif getal >= 6 and getal <= 8:
+        return ('zomer')
+    elif getal >= 9 and getal <= 11:
+        return ('herfst')
+    else:
+        return ('winter')
+
+
+getal = eval(input('Geeft getal op: '))
+print(seizoen(getal))
