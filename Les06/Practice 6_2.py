599d4628e80568e9e67b0ac28273a9097ef7e2a0
johnvanmeerten <john.vanmeerten@student.hu.nl>
2017-09-22 13:18:41 +0200

    cvbcvb

--- Les06/Practice 6_2.py
+++ Les06/Practice 6_2.py
@@ -3,5 +3,6 @@ woorden = eval(input("Geef lijst met minimaal 10 strings: "))
 tijdelijk = []
 
 for woord in woorden:
-    if len(woord) < 4:
-        woord = tijdelijk
+    if len(woord) <= 4:
+        tijdelijk.append(woord)
+print(tijdelijk)
