61744fc393416051d28f6475cc2d54df893aa82e
johnvanmeerten <john.vanmeerten@student.hu.nl>
2017-09-27 14:00:27 +0200

    cvbcvb

--- Les07/Practice 7_3.py
+++ Les07/Practice 7_3.py
@@ -1,7 +1,5 @@
 dict = {'Tim':8.2, 'Bob':7.2, 'Sanne':9.4, 'Ana':9.4, 'John':9.8, 'Pim':4.2, 'Elvira':7.2, 'Ramona':8.2}
 
-if dict.keys():
-    > 9:
-    print(keys)
-
-# hulp
+for naam, cijfer in dict.items():
+    if cijfer > 9:
+        print(naam, cijfer)
--- Les07/Practice 7_4.py
+++ Les07/Practice 7_4.py
@@ -1,1 +1,25 @@
-Begijp het inlezen van files niet goed genoeg
+def ticker(filename):
+    file = open(filename, 'r')
+    bedrijven = file.readlines()
+    newdect = {}
+
+    for bedrijf in bedrijven:
+        data = bedrijf.split(':')
+        newdect[data[0]] = data[1]
+    file.close()
+    return newdect
+
+def ticker1(newdect, input):
+    for tick in newdect:
+        if input == tick:
+            print('Ticker Symbol: {}'.format(newdect[tick]))
+        elif newdect[tick] == input:
+            print('Company name: {}'.format(tick))
+
+
+newdect = ticker('Beurs.txt')
+
+invoer = input('Hallo: ')
+
+ticker1(newdect, invoer)
+
