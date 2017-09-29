6aa3e100dadf8a06a5071af7005aee3868362243
johnvanmeerten <john.vanmeerten@student.hu.nl>
2017-09-29 10:38:29 +0200

    cvbcvb

---
+++ Les07/Oefenen.py
@@ -0,0 +1,9 @@
+woorden = ['Hogeschool', 'Utrecht', 'Hogeschool', 'Utrecht', 'Python'] 
+
+for woord in woorden:
+    if woord == 'Utrecht':
+        print(woord, end=' ')
+    else:
+        continue
+
+print('Hogeschool', end=' ')
