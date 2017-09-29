a489857c0557c0f7ddd1c49c7568bdbb2a23ae4b
johnvanmeerten <john.vanmeerten@student.hu.nl>
2017-09-29 15:56:08 +0200

    cvbcvb

--- Les08/Practice Exercise 8 _ 2.py
+++ Les08/Practice Exercise 8 _ 2.py
@@ -2,10 +2,16 @@ import random
 
 def monopolyworp():
     count = 0
+    while True:
+        count += 1
+        worp1 = random.randrange(1, 7)
+        worp2 = random.randrange(1, 7)
+        if worp1 == worp2:
+            print(worp1 + worp2)
+        else:
+            print()
 
-        while True:
-            count += 1
-            worp1 = random.randrange(1, 7)
-            worp2 = random.randrange(1, 7)
-            if worp1 == worp2:
-                print(worp1 + worp2)
+            break
+
+
+monopolyworp()
