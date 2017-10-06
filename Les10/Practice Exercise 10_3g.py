e96a957a911ca0f3d2c49d85817a0069884804f9
johnvanmeerten <john.vanmeerten@student.hu.nl>
2017-10-06 12:37:48 +0200

    cvbcvb

---
+++ Les10/Practice Exercise 10_3g.py
@@ -0,0 +1,15 @@
+x = 1
+y = 3
+def doe1():
+    global x
+    y = 4
+    x = 0
+    return x+y
+
+def doe2():
+    x = doe1()
+    x += 2
+    return x
+
+doe2()
+print(x)
--- Les10/Practice Exercise 10_3h.py
+++ Les10/Practice Exercise 10_3h.py
@@ -1,16 +1,12 @@
-x = 1
-y = 4
-
 def doe1():
-    global x
     y = 7
     x = 0
     return y
 
 def doe2():
+    global x
     x = doe1()
-    x +=1
-    return x
+    x += 1
 
-x = doe1()
+doe2()
 print(x)
