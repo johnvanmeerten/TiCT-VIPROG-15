f3e7f22142d7ea08b81d1172b30a0d023bad62ef
johnvanmeerten <john.vanmeerten@student.hu.nl>
2017-10-06 12:30:26 +0200

    cvbcvb

---
+++ Les10/Practice Exercise 10_3b.py
@@ -0,0 +1,11 @@
+x = 1
+y = 4
+
+def fun():
+    x = 2
+    global y
+    y = 3
+    print(y, end = ' ')
+
+fun()
+print(y, end = ' ')
---
+++ Les10/Practice Exercise 10_3c.py
@@ -0,0 +1,12 @@
+x = 2
+y = 5
+
+def fun():
+    y = 3
+    global x
+    x = 1
+    print(x*y, end = ' ')
+    return x*y
+
+x = fun()
+print(x*y, end = ' ')
