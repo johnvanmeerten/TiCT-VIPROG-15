c712c246252b8221a691aa0a762da3d4a0c4ebd8
johnvanmeerten <john.vanmeerten@student.hu.nl>
2017-10-10 12:12:31 +0200

    cvbcvb

--- Les10/Practice Exercise 10_1.py
+++ Les10/Practice Exercise 10_1.py
@@ -1,1 +1,15 @@
 import xmltodict
+
+def processXML(filename):
+    with open('magazijn.xml') as myXMLFile:
+        filestring = myXMLFile.read()
+        xmldictionary = xmltodict.parse(filestring)
+        return xmldictionary
+
+artikelendict = processXML('magazijn.xml')
+artikelen = artikelendict['artikelen']['artikel']
+
+print(artikelendict)
+
+for artikel in artikelen:
+    print(artikel['naam'])
