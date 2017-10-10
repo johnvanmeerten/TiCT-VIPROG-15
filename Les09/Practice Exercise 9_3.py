b62c76c667e479cab00024acdb50410e59aa111e
johnvanmeerten <john.vanmeerten@student.hu.nl>
2017-10-10 10:42:03 +0200

    cvbcvb

--- Les09/Practice Exercise 9_3.py
+++ Les09/Practice Exercise 9_3.py
@@ -1,5 +1,11 @@
 import csv
 with open('gamers.csv', 'r') as myCSVFile:
     reader = csv.reader(myCSVFile, delimiter = ';')
+    maxscore = 0
     for row in reader:
-        print(row[0], row[2])
+        score = int(row[2])
+        if score > maxscore:
+            maxscore = score
+            maxnaam = row[0]
+            maxdatum = row[1]
+    print('De hoogste score is: {} op {} behaald door {}'.format(maxscore, maxdatum, maxnaam))
